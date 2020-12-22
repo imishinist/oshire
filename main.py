28fb6236f846a94ab27c807ea7b56344affa33b4
Taisuke Miyazaki <imishinist@gmail.com>
2020-12-22 14:50:19 +0900

    install required package

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim 
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import numpy as np
+from sklearn.metrics import classification_report
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
 if __name__ == '__main__':
     print("Hello World")
